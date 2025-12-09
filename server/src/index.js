e0625f87266ed30e41dd3191d1a1981b4105da41
Superstaritik <165768296+Superstaritik@users.noreply.github.com>
2025-12-10 00:28:41 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,7 @@ app.use('/api/newsletter', newsletterRoutes);
 
 main()
 .then(async ()=>{
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server listening at port number : " + PORT);
 });
